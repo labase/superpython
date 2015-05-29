06a9e507e670aee4b174b7c4db8b814c86644182
cetoli <carlo@nce.ufrj.br>
2015-05-29 20:43:34 -0300

    feat(mig_util): added migration utility

--- src/server/controllers/mig_util.py
+++ src/server/controllers/mig_util.py
@@ -24,6 +24,86 @@ resources your app exposes to clients.
 
 """
 __author__ = 'carlo'
-import requests
+import urllib
 import json
-requests.get()
+from time import sleep
+USERS = "alpha bravo charlie echo golf hotel india" \
+        " kilo lima mike november oscar papa quebec sierra uniform zulu".split()
+
+class Util:
+    def __init__(self, user, passwd="nceufrj"):
+        self._token = 0
+        self._baseURL = "http://pyschool.net/"
+        self.user = user
+        self.passwd = passwd
+
+    def login(self):
+        sleep(0.5)
+        if True:
+            # send userid/password
+            _userid = self.user
+            _password = self.passwd
+            _url = "http://pyschool.net/Auth?userid=%s&password=%s" % (_userid, _password)
+
+            _fp = urllib.urlopen(_url)
+            _data = _fp.read()
+
+            _json = json.loads(_data)
+            if _json['status'] == 'Okay':
+                # import remote_storage_fs
+
+                # fs = remote_storage_fs.GoogleDataStorage("/pyschool")
+                self._token = _json['token']
+                _cookie = 'token=%s|login_type=authenticate' % _json['token']
+                return self
+            print(self.user, _json)
+            return self
+
+    def _remote_call(self, data):
+        # console.log("remote call", data)
+        data['token'] = self._token  # add in token to call
+        _json = json.dumps({'data': data})
+
+        try:
+            _fp = urllib.urlopen(self._baseURL + "FS", _json)
+            return json.loads(_fp.read())  # returns a string (in json format)
+        except:
+            return {'status': 'Error',
+                    'message': 'Network connectivity issues'}
+
+    def _read_file(self, filename):
+        """ retrieves file from storage, returns fileobj if successful,
+            return None if unsuccessful
+        """
+
+        _json = self._remote_call({'command': 'read_file', 'filename': filename})
+
+        try:
+            # _f = FileObject.FileObject()
+            # _f=FileSystemBase.FileObject()
+            # _f = json.load(_json['fileobj'])#["contents"]
+            _f = _json['fileobj']  # ["contents"]
+            _f = json.loads(_f)[u'contents']
+            return {'status': 'Okay', 'fileobj': _f}
+        except Exception as e:
+            return {'status': 'Error', 'message': str(e), 'json': _json, 'file':  filename}
+
+    def read_main_file(self, filename):
+        """ retrieves file from storage, returns fileobj if successful,
+            return None if unsuccessful
+        """
+        try:
+            cnt = self._read_file("/pyschool/%s" % filename)
+            return cnt['fileobj']
+        except Exception as e:
+            return {'status': 'Error', 'message': str(e), 'json': cnt}
+
+
+
+ps = Util('carlo', 'labase')
+print(ps.login()._token)
+#USERS =["alpha"]
+files = [Util(fl).login().read_main_file("a50/%s.py" % fl) for fl in USERS]
+# print(ps._read_file("/pyschool/adv.py")['fileobj'])
+for f in files:
+    print(f)
