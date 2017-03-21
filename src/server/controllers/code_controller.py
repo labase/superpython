16ad75941e0c6f368bc1fe9a457e3150025d8784
cetoli <carlo@ufrj.br>
2017-03-21 09:50:18 -0300

    bug(core,code): import now passes; fix unicode error at line 71, added u'' to string

--- src/server/controllers/code_controller.py
+++ src/server/controllers/code_controller.py
@@ -106,7 +106,7 @@ def handle(pypath):
 
     raise HTTPError(404, "No such module.")
 
-CODE_DEFAULT = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+CODE_DEFAULT = u"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
